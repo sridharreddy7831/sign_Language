3c0bd08ed25782d01c833b9f17c7e976160b391a
Nalipi Sridhar Reddy <108881367+sridharreddy7831@users.noreply.github.com>
2025-03-31 19:10:47 +0530

    Update and rename main.py to wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,4 @@
+from app import app
 import json
 import os
 # from nltk.parse import stanford
@@ -407,4 +408,4 @@ def serve_signfiles(path):
 
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0')
+    app.run()
